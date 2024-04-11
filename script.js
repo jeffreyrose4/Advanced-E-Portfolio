44c4a39822ae961dfa6fe525d3bcbed70c14f5b0
Jeffrey <jeferose4@gmail.com>
2024-04-11 11:45:46 -0600

    initialized git repository

---
+++ script.js
@@ -0,0 +1,24 @@
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+
+  emailjs
+    .sendForm(
+      "service_4sblk7c",
+      "template_pa5xt5c",
+      event.target,
+      'DCKiR8BE55Xe7MRvJ'
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly at jeferose4@gmail.com"
+      );
+    });
+}
